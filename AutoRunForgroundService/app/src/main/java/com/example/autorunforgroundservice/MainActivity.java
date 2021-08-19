4660938faef514cdbf1eeab3911c4cc7bc8b61fd
Md Mehedi Hasan <mhshobuj37@gmail.com>
2021-08-19 14:47:12 +0600

    auto run for adroid 10

---
+++ AutoRunForgroundService/app/src/main/java/com/example/autorunforgroundservice/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.autorunforgroundservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkOverlayPermission();
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+//            // Show alert dialog to the user saying a separate permission is needed
+//            // Launch the settings activity if the user prefers
+//            Intent myIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+//            startActivity(myIntent);
+//        }
+    }
+
+    // method to ask user to grant the Overlay permission
+    public void checkOverlayPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                // send user to the device settings
+                Intent myIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                startActivity(myIntent);
+            }
+        }
+    }
+}
---
+++ AutoRunForgroundService/app/src/main/java/com/example/autorunforgroundservice/MyBootCompletedReceiver.java
@@ -0,0 +1,23 @@
+package com.example.autorunforgroundservice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Objects;
+
+public class MyBootCompletedReceiver extends BroadcastReceiver {
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Objects.equals(intent.getAction(), Intent.ACTION_BOOT_COMPLETED)) {
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+}
